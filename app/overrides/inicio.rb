11eaea1c145fd8f9498897921db08393b383e24d
Angel Espinoza <a.espinozaflores@hotmail.com>
2017-03-29 08:36:50 -0600

    static content

--- app/overrides/inicio.rb
+++ app/overrides/inicio.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
                      :replace_contents => "#home-link",
-                     :text             => "<a href='public/ror'>RoR</a>",
+                     :text             => "<a href='public/ror'>Acerca de RoR</a>",
                      :name             => "change_link")
